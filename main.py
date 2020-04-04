1c67f9bace75299d2047bcc92389e0f5f3649019
bojthe <55790397+bojthe@users.noreply.github.com>
2020-04-04 14:08:28 +0300

    Check mate

--- main.py
+++ main.py
@@ -5,27 +5,120 @@ from termcolor import colored
 game_board = numpy.zeros((6, 7))
 full_columns = []
 
+
+def main():
+    global game_board
+
+
 def play_disc(player_num, column_num):
     global game_board
     global full_columns
     for i in reversed(range(0, 6)):
         if game_board[i][column_num] == 0:
             game_board[i][column_num] = player_num
+            if check(i, column_num, player_num):
+                pretty_print()
+                print("Player nr.{} has won!".format(player_num))
+                exit(0)
             if i == 0:
                 full_columns.append(column_num)
             return
 
 
-def main():
-    global game_board
+def check(i, j, p):
+    if (check_vertical_l(i, j - 1, p) + check_vertical_r(i, j + 1, p)) > 2:
+        return True
+    if (check_horiz_u(i - 1, j, p) + check_horiz_d(i + 1, j, p)) > 2:
+        return True
+    if (check_main_diag_l(i - 1, j - 1, p) + check_main_diag_r(i + 1, j + 1, p)) > 2:
+        return True
+    if (check_inv_diag_l(i + 1, j - 1, p) + check_inv_diag_r(i - 1, j + 1, p)) > 2:
+        return True
+    return False
 
 
-def pretty_print(active_player):
-    global game_board
-    if active_player:
-        print("\nMy turn!\n")
+def check_main_diag_l(i, j, p):
+    if i >= 0 and j >= 0:
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_main_diag_l(i - 1, j - 1, p)
     else:
-        print("\nYour turn!\n")
+        return 0
+
+
+def check_main_diag_r(i, j, p):
+    if i < len(game_board) and j < len(game_board[i]):
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_main_diag_r(i + 1, j + 1, p)
+    else:
+        return 0
+
+
+def check_inv_diag_r(i, j, p):
+    if i >= 0 and j < len(game_board[i]):
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_inv_diag_r(i - 1, j + 1, p)
+    else:
+        return 0
+
+
+def check_inv_diag_l(i, j, p):
+    if i < len(game_board) and j >= 0:
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_inv_diag_l(i + 1, j - 1, p)
+    else:
+        return 0
+
+
+def check_horiz_d(i, j, p):
+    if i < len(game_board):
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_horiz_d(i + 1, j, p)
+    else:
+        return 0
+
+
+def check_horiz_u(i, j, p):
+    if i >= 0:
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_horiz_u(i - 1, j, p)
+    else:
+        return 0
+
+
+def check_vertical_r(i, j, p):
+    if j < len(game_board[0]):
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_vertical_r(i, j + 1, p)
+    else:
+        return 0
+
+
+def check_vertical_l(i, j, p):
+    if j >= 0:
+        if game_board[i][j] != p:
+            return 0
+        else:
+            return 1 + check_vertical_l(i, j - 1, p)
+    else:
+        return 0
+
+
+def pretty_print():
+    global game_board
 
     print("  0   1   2   3   4   5   6  ")
     for row in game_board:
@@ -35,19 +128,8 @@ def pretty_print(active_player):
                 output += " "
             elif col == 1:
                 output += colored('O', 'red')
-                # output += "O"
             else:
                 output += colored('O', 'blue')
-                # output += "O"
             output += " | "
         print(output)
     print("|\/\/\/\/\/\/\/\/\/\/\/\/\/\|")
-
-
-# def check_if_over(game_board, )
-pretty_print(True)
-play_disc(1, 3)
-pretty_print(False)
-play_disc(2, 3)
-pretty_print(True)
-
