5bc1ab8a1c71abbf38a19797a99c459ca521f4a4
bojthe <55790397+bojthe@users.noreply.github.com>
2020-04-04 13:17:36 +0300

    asdfghjk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy
+import termcolor
+from termcolor import colored
 
 game_board = numpy.zeros((6, 7))
 full_columns = []
@@ -12,3 +14,40 @@ def play_disc(player_num, column_num):
             if i == 0:
                 full_columns.append(column_num)
             return
+
+
+def main():
+    global game_board
+
+
+def pretty_print(active_player):
+    global game_board
+    if active_player:
+        print("\nMy turn!\n")
+    else:
+        print("\nYour turn!\n")
+
+    print("  0   1   2   3   4   5   6  ")
+    for row in game_board:
+        output = "| "
+        for col in row:
+            if col == 0:
+                output += " "
+            elif col == 1:
+                output += colored('O', 'red')
+                # output += "O"
+            else:
+                output += colored('O', 'blue')
+                # output += "O"
+            output += " | "
+        print(output)
+    print("|\/\/\/\/\/\/\/\/\/\/\/\/\/\|")
+
+
+# def check_if_over(game_board, )
+pretty_print(True)
+play_disc(1, 3)
+pretty_print(False)
+play_disc(2, 3)
+pretty_print(True)
+
